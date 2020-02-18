7b97f39ca0821eb5c42ec7252ad5cbca075b9d18
endersonlg <endersonluciog@gmail.com>
2020-02-18 02:10:26 -0300

    Update UserController.js
    Implementado o update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,7 +16,25 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ error: 'funfando' });
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+    if (user.email !== email) {
+      const userExists = await User.findOne({ where: { email } });
+      if (userExists) {
+        res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
